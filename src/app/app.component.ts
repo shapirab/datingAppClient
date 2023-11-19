fc9ab44b5e718edd3732ae2fc508874eb69361ec
shapirab <shapirab@jamd.ac.il>
2023-11-19 10:12:26 +0200

    end of section 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { User } from './models/user';
+import { error } from 'console';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'client';
+  users: User[] | undefined;
+  url = 'https://localhost:7175/api/users';
+  constructor(private http: HttpClient){}
+
+  ngOnInit(): void {
+    this.http.get(this.url).subscribe({
+      next: res => this.users = res as User[],
+      error: err => console.log(err)
+    });
+
+  }
 }
